9348aa4fdf7c018b669332512aa8ada6330d810b
jsmartinezv <jsmartinezv@unal.edu.co>
2021-10-10 18:59:31 -0500

    valor de resistencia corregido

--- graficascalentamiento.py
+++ graficascalentamiento.py
@@ -6,22 +6,30 @@ m=1.77
 al=0.0039
 ce=385#1.7*pow(10, -8)
 I=387
-rf=0.309
+rf=0
+ro=0.309
 DTr=0
-DT=1
+DT=0
 n=0
-arr = np.array([0])
-while n<80:
-    rf=rf*(1 + al*DT)
+#arr = np.array([1,2,3])
+tiempo=[]
+temper=[]
+while n<1000:
+    rf=ro*(1 + al*DTr)
     t=(m*ce*DTr)/(pow(I, 2)*rf)
     n=n+1
     DTr=DTr+1
-    print('Temp: '+ str(DT) + '/ tiempo: '+ str(t) + ' Resist: '+ str(rf))
-    np.append(arr, t)
-    print(arr)
-   
-#plt.plot(DT, t)
-#plt.show()
+    print('Temp: '+ str(DTr+19) + '/ tiempo: '+ str(t) + ' Resist: '+ str(rf))
+    #np.append(arr, t)
+    Temp=DTr+19
+    tiempo.append(t)
+    temper.append(Temp)
+
+#print(tiempo)
+#print(temper)
+
+plt.plot(tiempo, temper)
+plt.show()
 #for i in range(0, 10, 2):
 #    l=m*i   
 #    print(l)

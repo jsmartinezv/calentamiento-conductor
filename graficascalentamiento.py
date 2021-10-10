aff47502daa4f59505258cb7b0c6b7ce50493515
jsmartinezv <jsmartinezv@unal.edu.co>
2021-10-10 18:23:15 -0500

    modificacion de funcionamiento, tiempo y resistencia bien calculados, faltan los arreglos para graficar

--- graficascalentamiento.py
+++ graficascalentamiento.py
@@ -1,20 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
 to=20
 tf=1000
 m=1.77
 al=0.0039
 ce=385#1.7*pow(10, -8)
 I=387
-ro=0.309
+rf=0.309
 DTr=0
 DT=1
 n=0
+arr = np.array([0])
 while n<80:
-    rf=ro*(1+al*DTr)
-    t=(m*ce*DT)/(pow(I, 2)*rf)
+    rf=rf*(1 + al*DT)
+    t=(m*ce*DTr)/(pow(I, 2)*rf)
     n=n+1
-    DT=DT+1
+    DTr=DTr+1
     print('Temp: '+ str(DT) + '/ tiempo: '+ str(t) + ' Resist: '+ str(rf))
-    
+    np.append(arr, t)
+    print(arr)
+   
+#plt.plot(DT, t)
+#plt.show()
 #for i in range(0, 10, 2):
 #    l=m*i   
 #    print(l)

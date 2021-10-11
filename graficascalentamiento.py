56bce7a521947dea6d94235bbcde2b1ab5520631
jsmartinezv <jsmartinezv@unal.edu.co>
2021-10-10 21:44:16 -0500

    se cambio la estructura se revisaron los datos y se impremen dos graficas con los respectivos nombres en los ejes y el titulo

--- graficascalentamiento.py
+++ graficascalentamiento.py
@@ -1,35 +1,33 @@
 import matplotlib.pyplot as plt
 import numpy as np
-to=20
-tf=1000
-m=1.77
-al=0.0039
-ce=385#1.7*pow(10, -8)
-I=387
-rf=0
-ro=0.309
-DTr=0
-DT=0
-n=0
-#arr = np.array([1,2,3])
-tiempo=[]
+m=1.77    # masa
+al=0.0039 # coeficiente termico de resistividad cobre 
+ce=385    # calor especifico cobre
+I=387     # I_cc
+rf=0      
+ro=0.309  # resistencia conductor a temperatura ambiente   
+DTr=0     # Delta de temperatura     
+tiempo=[] ### inicializacion arrays usados para graficar
 temper=[]
-while n<1000:
-    rf=ro*(1 + al*DTr)
-    t=(m*ce*DTr)/(pow(I, 2)*rf)
-    n=n+1
+resist=[]
+while DTr<1000: # bucle de iteración
+    rf=ro*(1 + al*DTr)  # calculo de la resistencia variable
+    t=(m*ce*DTr)/(pow(I, 2)*rf) # calculo del tiempo de calentamiento
     DTr=DTr+1
     print('Temp: '+ str(DTr+19) + '/ tiempo: '+ str(t) + ' Resist: '+ str(rf))
-    #np.append(arr, t)
     Temp=DTr+19
-    tiempo.append(t)
+    tiempo.append(t)  ### agregar datos a los arreglos
     temper.append(Temp)
-
-#print(tiempo)
-#print(temper)
-
+    resist.append(rf)
+# grafico 1 temperatura - resistencia
+plt.title("Efecto de la temperatura sobre la resistencia")
+plt.xlabel("Temperatura [^oC]")
+plt.ylabel("Resistencia [Ohms]")
+plt.plot(temper, resist)
+plt.show()
+# grafico 2 temperatura - tiempo
+plt.title("Curva caracteristica: tiempo de calentamiento de línea de tx \n Cu, 60 m , 12 AWG, 387 A ")
+plt.ylabel("Temperatura [^oC]")
+plt.xlabel("Tiempo [s]")
 plt.plot(tiempo, temper)
 plt.show()
-#for i in range(0, 10, 2):
-#    l=m*i   
-#    print(l)
